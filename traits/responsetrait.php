0ee3584fb53650695e809c2689fb9b8d54c9972f
Chris Zuber <shgysk8zer0@gmail.com>
2020-06-23 11:16:14 -0700

    Add missing `Response::statusText` values
    Still many missing, but have more common statuses, as well as the longest one so that no further alignment adjustments will be needed.

--- traits/responsetrait.php
+++ traits/responsetrait.php
@@ -84,22 +84,27 @@ trait ResponseTrait
 	{
 		// @TODO Finish filling out status code text
 		switch($this->getStatus()) {
-			case HTTP::CONT: return 'Continue';
-			case HTTP::SWITCHING_PROTOCOLS: return 'Switching Protocols';
-			case HTTP::PROCESSING: return 'Processing';
-			case HTTP::OK: return 'Ok';
-			case HTTP::CREATED: return 'Created';
-			case HTTP::ACCEPTED: return 'Accepted';
-			case HTTP::NON_AUTHORITATIVE: return 'Non-Authoritative';
-			case HTTP::NO_CONTENT: return 'No Content';
-			case HTTP::BAD_REQUEST: return 'Bad Request';
-			case HTTP::UNAUTHORIZED: return 'Unauthorized';
-			case HTTP::PAYMENT_REQUIRED: return 'Payment Required';
-			case HTTP::FORBIDDEN: return 'Forbidden';
-			case HTTP::NOT_FOUND: return 'Not Found';
-			case HTTP::METHOD_NOT_ALLOWED: return 'Not Found';
-			case HTTP::INTERNAL_SERVER_ERROR: return 'Internal Server Error';
-			default: return 'Unknown';
+			case HTTP::CONT:                            return 'Continue';
+			case HTTP::SWITCHING_PROTOCOLS:             return 'Switching Protocols';
+			case HTTP::PROCESSING:                      return 'Processing';
+			case HTTP::OK:                              return 'Ok';
+			case HTTP::CREATED:                         return 'Created';
+			case HTTP::ACCEPTED:                        return 'Accepted';
+			case HTTP::NON_AUTHORITATIVE:               return 'Non-Authoritative';
+			case HTTP::NO_CONTENT:                      return 'No Content';
+			case HTTP::BAD_REQUEST:                     return 'Bad Request';
+			case HTTP::UNAUTHORIZED:                    return 'Unauthorized';
+			case HTTP::PAYMENT_REQUIRED:                return 'Payment Required';
+			case HTTP::FORBIDDEN:                       return 'Forbidden';
+			case HTTP::NOT_FOUND:                       return 'Not Found';
+			case HTTP::METHOD_NOT_ALLOWED:              return 'Not Found';
+			case HTTP::NOT_IMPLEMENTED:                 return 'Not Implemented';
+			case HTTP::BAD_GATEWAY:                     return 'Bad Gateway';
+			case HTTP::REQUEST_HEADER_FILEDS_TOO_LARGE: return 'Request Header Fields Too Large';
+			case HTTP::INTERNAL_SERVER_ERROR:           return 'Internal Server Error';
+			case HTTP::SERVICE_UNAVAILABLE:             return 'Service Unavailable';
+			case HTTP::GATEWAY_TIMEOUT:                 return 'Gateway Timeout';
+			default:                                    return 'Unknown';
 		}
 	}
 
